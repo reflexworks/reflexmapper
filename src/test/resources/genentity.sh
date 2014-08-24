303c920edee27c79b48584204c90510663b88705
takezaki <stakezaki@gmail.com>
2014-08-24 15:24:00 +0900

    安全性に配慮

--- src/test/resources/genentity.sh
+++ src/test/resources/genentity.sh
@@ -2,7 +2,7 @@
 #./genentity.sh {service_name} {prop_acls} {outputpath} {secretkey}
 current=$(cd $(dirname $0);pwd)
 
-rm -rf $1
+#rm -rf $1
 rm src/main/webapps/WEB-INF/lib/entity.jar
 
 java -cp $current/src/main/webapps/WEB-INF/lib/reflexcore-2.1.2.jar:$current/target/test-classes:$current/src/main/resources:$current/src/main/webapps/WEB-INF/lib/reflexmapper-2.1.2.jar:$current/src/main/webapps/WEB-INF/lib/xstream-1.1.2.jar:$current/src/main/webapps/WEB-INF/lib/xpp3-1.1.3.4-RC8.jar:$current/src/main/webapps/WEB-INF/lib/json-20070829.jar:$current/src/main/webapps/WEB-INF/lib/javassist-3.18.0-GA.jar:$current/src/main/webapps/WEB-INF/lib/msgpack-0.6.8.jar jp.reflexworks.atom.util.GenerateEntity $2 $3 $4
