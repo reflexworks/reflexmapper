043b6925e905da77a84bb3ebbd0bfe3680ff7ca5
noriko-terada <terada@virtual-tech.net>
2014-10-03 13:16:36 +0900

    テストクラスの項目の先頭アンダースコアを削除。 自動生成されるメソッドの追加。

--- src/test/java/jp/reflexworks/test2/model/Comment.java
+++ src/test/java/jp/reflexworks/test2/model/Comment.java
@@ -2,15 +2,16 @@ package jp.reflexworks.test2.model;
 
 public class Comment {
 	
-	public String _nickname;
 	public String _$$text;
+	public String nickname;
+	public String secret;
 
 	public String getNickname() {
-		return _nickname;
+		return nickname;
 	}
 
 	public void setNickname(String nickname) {
-		this._nickname = nickname;
+		this.nickname = nickname;
 	}
 
 	public String get$$text() {
@@ -21,9 +22,23 @@ public class Comment {
 		this._$$text = _$$text;
 	}
 
-	@Override
-	public String toString() {
-		return "Comment [nickname=" + _nickname + ", _$$text=" + _$$text + "]";
+	public String getSecret() {
+		return secret;
 	}
 
+	public void setSecret(String secret) {
+		this.secret = secret;
+	}
+
+	@Override
+	public String toString() {
+		return "Comment [nickname=" + nickname + ", $$text=" + _$$text + " secret=" + secret + "]";
+	}
+	
+	public Object getValue(String fldname) {if (fldname.equals("comment.$$text")) return _$$text;if (fldname.equals("comment.nickname")) return nickname;return null;}
+	public void encrypt(String id, Object cipher, String secretkey) {if (secret!=null)secret=(String) jp.reflexworks.atom.mapper.CipherUtil.doEncrypt(""+secret, "testsecret123"+id, cipher);}
+	public void decrypt(String id, Object cipher, String secretkey) {if (secret!=null)secret=(String) jp.reflexworks.atom.mapper.CipherUtil.doDecrypt(""+secret, "testsecret123"+id, cipher);}	public void isMatch(jp.reflexworks.atom.mapper.ConditionContext context) {if (_$$text!=null) {context.fldname="comment.$$text";context.type="String";context.obj=_$$text;jp.reflexworks.atom.mapper.ConditionContext.checkCondition(context);}if (nickname!=null) {context.fldname="comment.nickname";context.type="String";context.obj=nickname;jp.reflexworks.atom.mapper.ConditionContext.checkCondition(context);}};
+	public boolean validate(String uid, java.util.List groups, String myself) throws java.text.ParseException {return true;};
+	public void maskprop(String uid, java.util.List groups, String myself) {};
+	
 }
--- src/test/java/jp/reflexworks/test2/model/Feed.java
+++ src/test/java/jp/reflexworks/test2/model/Feed.java
@@ -10,33 +10,10 @@ public class Feed extends FeedBase implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	public String _$xmlns = "http://www.w3.org/2005/Atom";
-
-	// ---- 名前空間定義 ----
-	public String _$xmlns$vt = "http://reflexworks.jp/test/1.0";
-
-	public String get_$xmlns() {
-		return _$xmlns;
-	}
-	public void set_$xmlns(String _$xmlns) {
-		this._$xmlns = _$xmlns;
-	}
-	public String get_$xmlns$vt() {
-		return _$xmlns$vt;
-	}
-	public void set_$xmlns$vt(String _$xmlns$vt) {
-		this._$xmlns$vt = _$xmlns$vt;
-	}
+	public Object getValue(String fldname) {if (entry!=null) for (int i=0;i<entry.size();i++) { Object value =((jp.reflexworks.atom.entry.SoftSchema)entry.get(i)).getValue(fldname);if (value!=null) return value;}return null;};
+	public void encrypt(String id, Object cipher, String secretkey) {if (entry!=null) for (int i=0;i<entry.size();i++) { ((jp.reflexworks.atom.entry.SoftSchema)entry.get(i)).encrypt(id, cipher, secretkey);}};
+	public void decrypt(String id, Object cipher, String secretkey) {if (entry!=null) for (int i=0;i<entry.size();i++) { ((jp.reflexworks.atom.entry.SoftSchema)entry.get(i)).decrypt(id, cipher, secretkey);}};
+	public boolean validate(String uid, java.util.List groups) throws java.text.ParseException {String myself = null;if (entry!=null&&100<entry.size()) throw new java.text.ParseException("Maximum number of 'entry' exceeded.",0);if (entry!=null) for (int i=0;i<entry.size();i++) { ((jp.reflexworks.atom.entry.EntryBase)entry.get(i)).validate(uid,groups);}return true;};
+	public void maskprop(String uid, java.util.List groups) {String myself = null;if (entry!=null) for (int i=0;i<entry.size();i++) { ((jp.reflexworks.atom.entry.EntryBase)entry.get(i)).maskprop(uid,groups);}};
 	
-	@Override
-	public boolean validate(String ucode, List groups) throws ParseException {
-		// TODO 自動生成されたメソッド・スタブ
-		return false;
-	}
-	@Override
-	public void maskprop(String ucode, List groups) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-	}
-
 }
