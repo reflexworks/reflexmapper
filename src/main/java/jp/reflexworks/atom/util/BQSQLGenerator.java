d8b60777e2614d32a82ad3642bdb304692d67519
takezaki <stakezaki@gmail.com>
2015-07-12 15:42:48 +0900

    BQSQLGenerator

--- src/main/java/jp/reflexworks/atom/util/BQSQLGenerator.java
+++ src/main/java/jp/reflexworks/atom/util/BQSQLGenerator.java
@@ -1,17 +1,25 @@
 package jp.reflexworks.atom.util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
 import jp.reflexworks.atom.mapper.FeedTemplateMapper.Meta;
+import jp.reflexworks.atom.wrapper.Condition;
 
 public class BQSQLGenerator {
 
 	private List<Meta> metalist;
 	private Set<String> repeatedItemSet;
 
-	public String generate(List<Meta> metalist, String table) {
+	private String[] primitives = {"___key", "___revision", "id", "rights____type", "link.___rel", "link.___href"};
+	
+	public String generate(List<Meta> metalist, String table,Condition[] conditions,String linkhref) {
 		this.metalist = metalist;
 
 		StringBuilder sb = new StringBuilder();
@@ -27,10 +35,74 @@ public class BQSQLGenerator {
 			addRepeatedItems(item, sb, table);
 		}
 
+		sb.append(") src\n");
+		sb.append("JOIN EACH\n");
+		sb.append("(select id from\n");
+		
+		List<Condition> conditionlist = Arrays.asList(conditions);
+
+		Set<String> nestset = new LinkedHashSet<String>();
+		for(Condition condition:conditionlist) {
+			nestset.addAll(getParentList(condition.getProp()));
+		}
+		nestset.add("link");
+		addNest(nestset,conditionlist,table,sb);
+		
+		sb.append("JOIN EACH\n");
+		sb.append("(select max(___revision) as max____revision, ___key as max____key from ["+table+"] \n");
+		sb.append("GROUP EACH BY max____key) mx \n");
+		sb.append("ON nest_src.___revision=mx.max____revision and nest_src.___key=mx.max____key \n");
+		sb.append("where (link.___rel=\"self\" or link.___rel=\"alternate\") \n");
+		sb.append("and regexp_match(link.___href, '^"+linkhref+"[^/]+$') \n");
+		sb.append("and rights____type is null \n");
+		
+		for(Condition condition:conditionlist) {
+			sb.append("and regexp_match("+condition.getProp()+",\"^"+condition.getValue()+"$\") \n");
+		}
+		sb.append("GROUP EACH by id) grp \n");
+		sb.append("ON src.id = grp.id \n");
+		
+		sb.append("GROUP BY "+trailer1(line)+" ");
+		sb.append("ORDER BY "+trailer2(getLineNum())+" ");
+
 		return sb.toString();
 
 	}
 
+	private void addNest(Set<String> nestset,List<Condition> conditionlist,String table,StringBuilder sb) {
+
+		String nestitems = getNestItems(conditionlist);
+		for(String nestflats:nestset) {
+			sb.append("(FLATTEN((select ");
+			sb.append(nestitems);
+			sb.append(" from \n");
+		}
+		sb.append("["+table+"]),");
+		Iterator<String> it = nestset.iterator();
+        while (it.hasNext()) {
+			sb.append(it.next()+")))");
+			if (it.hasNext()) {
+				sb.append(",\n");
+			}else {
+				sb.append(" nest");
+			}
+        }		
+	}
+	
+	private String getNestItems(List<Condition> conditionlist) {
+		StringBuilder sb = new StringBuilder();
+		List<String> items = new ArrayList<String>();
+		items.addAll(Arrays.asList(primitives));
+		for(Condition condition:conditionlist) {
+			items.add(condition.getProp());
+		}
+		for(int i=0;i<items.size();i++) {
+			sb.append(items.get(i));
+			if (i<items.size()-1) sb.append(",");
+		}
+		return sb.toString();
+	}
+	
 	private void addRepeatedItems(String item, StringBuilder sb, String table) {
 		String token = "id," + getNums(item) + "," + getItems(item);
 		if (isRepeated(getParent(item))) {
@@ -41,6 +113,17 @@ public class BQSQLGenerator {
 			sb.append("(FLATTEN((select '" + item + "' as flg, " + token
 					+ " from [" + table + "])," + item + ")),\n");
 		}
+	}
+	
+	private List<String> getParentList(String item) {
+		List<String> result = new ArrayList<String>();
+		while(true) {
+			item = getParent(item);
+			if (item.equals("")) break;
+			result.add(item);
+		}
+		Collections.reverse(result);
+		return result;
 	}
 
 	private String getParent(String item) {
@@ -116,11 +199,30 @@ public class BQSQLGenerator {
 				+ org.replace(",updated", "").replace("id,", "");
 	}
 
+	private String trailer1(String org) {
+		return "flg,src.id, id, updated, rights____type,"
+				+ org.replace("id,", "").replace("updated,", "")
+						.replace("rights____type,", "");
+	}
+
+	private String trailer2(String org) {
+		return "src.id, flg"+org;
+	}
+
 	private String getLine() {
 
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < metalist.size(); i++) {
 			outAll(metalist.get(i), sb, (i < metalist.size() - 1));
+		}
+		return sb.toString();
+	}
+
+	private String getLineNum() {
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < metalist.size(); i++) {
+			outNum(metalist.get(i), sb, (i < metalist.size() - 1));
 		}
 		return sb.toString();
 	}
@@ -158,6 +260,17 @@ public class BQSQLGenerator {
 		} else {
 			if (meta.repeated && !meta.self.equals("entry")) {
 				sb.append(meta.name + ".___num,");
+			}
+		}
+
+	}
+
+	private void outNum(Meta meta, StringBuilder sb, boolean comma) {
+
+		if (!meta.isrecord && !meta.name.equals("content.$$text")) {
+		} else {
+			if (meta.repeated && !meta.self.equals("entry")) {
+				sb.append(","+meta.name + ".___num");
 			}
 		}
 
--- src/test/java/jp/reflexworks/atom/test/TestBQSQLGenerator.java
+++ src/test/java/jp/reflexworks/atom/test/TestBQSQLGenerator.java
@@ -8,6 +8,7 @@ import java.util.zip.DataFormatException;
 
 import jp.reflexworks.atom.mapper.FeedTemplateMapper;
 import jp.reflexworks.atom.util.BQSQLGenerator;
+import jp.reflexworks.atom.wrapper.Condition;
 import jp.sourceforge.reflex.exception.JSONException;
 
 import org.junit.Test;
@@ -46,10 +47,32 @@ public class TestBQSQLGenerator {
 	public void testBQSQLGenerator() throws ParseException, JSONException, IOException, DataFormatException, ClassNotFoundException {
 		FeedTemplateMapper mp = new FeedTemplateMapper(entitytemplbq, SECRETKEY);		
 		BQSQLGenerator generator = new BQSQLGenerator();
-		String schema = generator.generate(mp.getMetalist("service_name"),"test_dataset.test_listnum_table");
+		
+		Condition[] conditions = new Condition[3];
+		conditions[0] = new Condition("user_pine.user_bamboo.user_plum.plum_text", "梅3-2-2");
+		conditions[1] = new Condition("user_parent.user_child.child_text", "子項目2-1の2");
+		conditions[2] = new Condition("title", "ユーザ項目");
+
+		String linkhref ="/rrr/";
+		
+		String schema = generator.generate(mp.getMetalist("service_name"),"test_dataset.test_listnum_table",conditions,linkhref);
 		System.out.println("\n=== BigQuery SQL ===");
 		System.out.println(schema);
 		
 	}
 
+	@Test
+	public void testBQ()  {
+		Condition condition = new Condition("author.name-ge-2014/10/03");
+		System.out.println(condition.getProp());
+		System.out.println(condition.getValue());
+
+		condition = new Condition("subInfo.favorite.music", "ポップス1");
+		System.out.println(condition.getProp());
+		System.out.println(condition.getValue());
+
+	}
+	
+	
+	
 }
