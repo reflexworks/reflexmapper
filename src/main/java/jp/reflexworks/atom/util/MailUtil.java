08a8caeeec8f59bd1effc5090d5d509c777c3593
terada <terada@virtual-tech.net>
2017-11-28 13:33:06 +0900

    メール送信機能のHTMLメール、インライン画像、添付ファイル対応。

--- src/main/java/jp/reflexworks/atom/util/MailUtil.java
+++ src/main/java/jp/reflexworks/atom/util/MailUtil.java
@@ -5,77 +5,133 @@ import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.Properties;
 import java.util.Map;
-import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Logger;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeUtility;
 import javax.mail.internet.MimeBodyPart;
 
 import jp.reflexworks.atom.api.MailConst;
 import jp.sourceforge.reflex.util.StringUtils;
 
+/**
+ * メール送信ユーティリティ
+ */
 public class MailUtil implements MailConst {
 
-	public static final Map<String, String> HEADERS = new HashMap<String, String>();
-	static {
-		HEADERS.put("Content-Transfer-Encoding", "7bit");
-	}
-
+	/** ロガー */
 	private static Logger logger = Logger.getLogger(MailUtil.class.getName());
 
-	public void send(String title, String message, String to, String from, 
-			String user, String password, String host, String port, String protocol,
-			boolean isStarttls, boolean debug) 
+	/**
+	 * テキストメール送信
+	 * @param title 題名
+	 * @param textMessage テキストメッセージ
+	 * @param to 送信先アドレス
+	 * @param from 送信元アドレス(認証にも使用)
+	 * @param password 認証パスワード
+	 * @param host SMTPサーバ
+	 * @param port ポート番号
+	 * @param protocol プロトコル(smtpまたはsmtps)
+	 * @param debug デバッグログを出力する場合true
+	 */
+	public static void send(String title, String textMessage, String to, String from, 
+			String password, String host, String port, String protocol,
+			boolean debug) 
 	throws IOException {
-		send(title, message, to, null, from, null, user, password, host, port, protocol, 
-				isStarttls, debug);
+		send(title, textMessage, null, null, null, 
+				to, null, from, null, null, password, host, port, protocol, 
+				null, null, debug);
 	}
-
-	public void send(String title, String message, String to, String toPersonal,
-			String from, String fromPersonal, String user, String password, 
-			String host, String port, String protocol, boolean isStarttls, boolean debug) 
+	
+	/**
+	 * メール送信
+	 * @param title 題名
+	 * @param textMessage テキストメッセージ
+	 * @param htmlMessage HTMLメッセージ
+	 * @param inlineImages インライン画像
+	 * @param attachments 添付ファイル
+	 * @param to 送信先アドレス
+	 * @param toPersonal 送信先名
+	 * @param from 送信元アドレス
+	 * @param fromPersonal 送信元名
+	 * @param user 認証ユーザ(指定のない場合は送信元アドレスが認証ユーザとなる。)
+	 * @param password 認証パスワード
+	 * @param host SMTPホスト
+	 * @param port SMTPポート番号
+	 * @param protocol プロトコル(smtpまたはsmtps)
+	 * @param isStarttls STARTTLSを行う場合true(デフォルトはtrue)
+	 * @param isAuth 認証を行う場合true(デフォルトはtrue)
+	 * @param debug デバッグログを出力する場合true
+	 */
+	public static void send(String title, String textMessage, String htmlMessage,
+			Map<String, DataSource> inlineImages, List<DataSource> attachments,
+			String to, String toPersonal, String from, String fromPersonal, 
+			String user, String password,  String host, String port, String protocol, 
+			Boolean isStarttls, Boolean isAuth, boolean debug) 
 	throws IOException {
 		Properties props = new Properties();
 		props.put(PROP_SMTP_HOST, host);
 		props.put(PROP_HOST, host);
 		props.put(PROP_FROM, from);
 		props.put(PROP_SMTP_PORT, port);  // サブミッションポート
-		props.put(PROP_SMTP_AUTH, "true");   // SMTP 認証を行う
-		props.put(PROP_SMTP_STARTTLS, "true");   // STARTTLS
+		if (isStarttls != null && isAuth) {
+			props.put(PROP_SMTP_AUTH, "true");   // SMTP 認証を行う
+		}
+		if (isStarttls != null && isStarttls) {
+			props.put(PROP_SMTP_STARTTLS, "true");   // STARTTLS
+		}
 		if (debug) {
 			props.put(PROP_DEBUG, "true");
 		}
-
-		send(title, message, to, toPersonal, from, fromPersonal, user, password, props, 
+		
+		send(title, textMessage, htmlMessage, inlineImages, attachments, 
+				to, toPersonal, from, fromPersonal, user, password, props, 
 				protocol, debug);
 	}
 
-	public void send(String title, String message, String to, String from, String user, 
-			String password, Properties props, String protocol, boolean debug) 
-	throws IOException {
-		send(title, message, to, null, from, null, user, password, props, protocol, debug);
-	}
-
-	public void send(String title, String message, String to, String toPersonal,
-			String from, String fromPersonal, String user, String password, Properties props, 
-			String protocol, boolean debug) 
+	/**
+	 * メール送信
+	 * @param title 題名
+	 * @param textMessage テキストメッセージ
+	 * @param htmlMessage HTMLメッセージ
+	 * @param inlineImages インライン画像
+	 * @param attachments 添付ファイル
+	 * @param to 送信先アドレス
+	 * @param toPersonal 送信先名
+	 * @param from 送信元アドレス
+	 * @param fromPersonal 送信元名
+	 * @param user 認証ユーザ(指定のない場合は送信元アドレスが認証ユーザとなる。)
+	 * @param password 認証パスワード
+	 * @param props プロパティ
+	 * @param protocol プロトコル(smtpまたはsmtps)
+	 * @param debug デバッグログを出力する場合true
+	 */
+	public static void send(String title, String textMessage, String htmlMessage,
+			Map<String, DataSource> inlineImages, List<DataSource> attachments,
+			String to, String toPersonal,
+			String from, String fromPersonal, String user, String password, 
+			Properties props, String protocol, boolean debug) 
 	throws IOException {
 		Session session = Session.getInstance(props);
 		session.setDebug(debug);
-
+		
 		Transport transport = null;
 		try {
 			InternetAddress iaTo = new InternetAddress(to, toPersonal, CHARSET);
 			InternetAddress iaFrom = new InternetAddress(from, fromPersonal, CHARSET);
-
+			
 			MimeMessage msg = new MimeMessage(session);
-
+			
 			// From
 			msg.setFrom(iaFrom);
 			// To
@@ -84,34 +140,89 @@ public class MailUtil implements MailConst {
 			msg.setSubject(title, CHARSET);
 			// Date
 			msg.setSentDate(new Date());
-			// 本文
-			/*
-			//msg.setContent(message, CONTENT_TYPE);
-			String encodeMessage = MimeUtility.encodeText(message, CHARSET, ENCODING);
-			msg.setContent(encodeMessage, CONTENT_TYPE);
-			 */
-
-			MimeMultipart multipart = new MimeMultipart();
-			msg.setContent(multipart);
-			MimeBodyPart body = new MimeBodyPart();
-			//body.setContent(message, CONTENT_TYPE);
-
-			if (!StringUtils.isBlank(protocol)) {
-				// 通常のメール送信
-				String jisMsg = convertJIS(message);
-				body.setContent(jisMsg, CONTENT_TYPE);
+			
+			// メインパート
+			// HTML形式 + インライン画像 + 添付ファイルの場合、mixed
+			// その他はalternative
+			Multipart alternativePart = new MimeMultipart(ALTERNATIVE);
+			Multipart mainPart = null;
+			if (isMixedPart(htmlMessage, inlineImages, attachments)) {
+				// mixed
+				mainPart = new MimeMultipart(MIXED);
+				
+				// alternative
+				MimeBodyPart alternativeBodyPart = new MimeBodyPart();
+				alternativeBodyPart.setContent(alternativePart);
+				mainPart.addBodyPart(alternativeBodyPart);
+				
 			} else {
-				// GAE
-				body.setText(message);
+				// alternative
+				mainPart = alternativePart;
 			}
-
-			multipart.addBodyPart(body);
-
-			// ヘッダ
-			for (Map.Entry<String, String> mapEntry : HEADERS.entrySet()) {
-				msg.setHeader(mapEntry.getKey(), mapEntry.getValue());
+			
+			// text mail
+			MimeBodyPart textBodyPart = new MimeBodyPart();
+			textBodyPart.setText(StringUtils.null2blank(textMessage), CHARSET, PLAIN);
+			textBodyPart.setHeader(CONTENT_TRANSFER_ENCODING, BASE64);
+			alternativePart.addBodyPart(textBodyPart);
+			
+			// html mail
+			if (isSendHtmlMessage(htmlMessage)) {
+				boolean isSendInlineImages = isSendInlineImages(htmlMessage, 
+						inlineImages);
+				Multipart relatedPart = null;
+				if (isSendInlineImages) {
+					// related
+					MimeBodyPart relatedBodyPart = new MimeBodyPart();
+					relatedPart = new MimeMultipart(RELATED);
+					relatedBodyPart.setContent(relatedPart);
+					alternativePart.addBodyPart(relatedBodyPart);
+				} else {
+					// alternative
+					relatedPart = alternativePart;
+				}
+				
+				// html message
+				MimeBodyPart htmlBodyPart = new MimeBodyPart();
+				htmlBodyPart.setText(htmlMessage, CHARSET, HTML);
+				htmlBodyPart.setHeader(CONTENT_TRANSFER_ENCODING, BASE64);
+				relatedPart.addBodyPart(htmlBodyPart);
+				
+				// inline image
+				if (isSendInlineImages) {
+					for (Map.Entry<String, DataSource> mapEntry : inlineImages.entrySet()) {
+						String cid = mapEntry.getKey();
+						DataSource dataSource = mapEntry.getValue();
+						MimeBodyPart imageBodyPart = new MimeBodyPart();
+						DataHandler dataHandler= new DataHandler(dataSource);
+						imageBodyPart.setDataHandler(dataHandler);
+						String fileName = dataSource.getName();
+						if (!StringUtils.isBlank(fileName)) {
+							imageBodyPart.setFileName(MimeUtility.encodeWord(dataSource.getName()));
+						}
+						imageBodyPart.setDisposition(MimeBodyPart.INLINE);	    // inline指定しておく
+						imageBodyPart.setContentID("<" + cid + ">");  // インライン画像を指定
+						relatedPart.addBodyPart(imageBodyPart);
+					}
+				}
 			}
-
+			
+			// attach image
+			if (isSendAttachments(attachments)) {
+				for (DataSource dataSource : attachments) {
+					MimeBodyPart attachBodyPart = new MimeBodyPart();
+					DataHandler dataHandler2 = new DataHandler(dataSource);
+					attachBodyPart.setDataHandler(dataHandler2);
+					String fileName = dataHandler2.getName();
+					attachBodyPart.setFileName(MimeUtility.encodeWord(fileName));
+					attachBodyPart.setDisposition(MimeBodyPart.ATTACHMENT);  // attachment 指定しておく
+					mainPart.addBodyPart(attachBodyPart);
+				}
+			}
+			
+			// set mainPart
+			msg.setContent(mainPart);
+			
 			if (!StringUtils.isBlank(protocol)) {
 				// protocolが設定されている場合、認証コネクト
 				transport = session.getTransport(protocol);
@@ -125,9 +236,7 @@ public class MailUtil implements MailConst {
 				// protocolが設定されていない場合、そのままsendする。(GAE)
 				Transport.send(msg);
 			}
-
-			//logger.info("send mail.");
-
+			
 		} catch (MessagingException e) {
 			IOException ie = new IOException(e.getMessage());
 			ie.initCause(e);
@@ -143,8 +252,57 @@ public class MailUtil implements MailConst {
 			}
 		}
 	}
-
-	public String convertJIS(String msg) {
+	
+	/**
+	 * HTMLメッセージを送るかどうか判定
+	 * @param htmlMessage HTMLメッセージ
+	 * @return HTMLメッセージを送る場合true
+	 */
+	private static boolean isSendHtmlMessage(String htmlMessage) {
+		return !StringUtils.isBlank(htmlMessage);
+	}
+	
+	/**
+	 * インライン画像を送るかどうか判定
+	 * @param htmlMessage HTMLメッセージ
+	 * @param inlineImages インライン画像
+	 * @return インライン画像を送る場合true
+	 */
+	private static boolean isSendInlineImages(String htmlMessage, 
+			Map<String, DataSource> inlineImages) {
+		return !StringUtils.isBlank(htmlMessage) &&
+				inlineImages != null && !inlineImages.isEmpty();
+	}
+	
+	/**
+	 * 添付ファイルを送るかどうか判定
+	 * @param attachments 添付ファイル
+	 * @return 添付ファイルを送る場合true
+	 */
+	private static boolean isSendAttachments(List<DataSource> attachments) {
+		return attachments != null && !attachments.isEmpty();
+	}
+	
+	/**
+	 * mixed partかどうか判定.
+	 * 以下の引数が全て設定されている場合true
+	 * @param htmlMessage HTMLメッセージ
+	 * @param inlineImages インライン画像
+	 * @param attachments 添付ファイル
+	 * @return mixed partの場合true
+	 */
+	private static boolean isMixedPart(String htmlMessage, 
+			Map<String, DataSource> inlineImages, List<DataSource> attachments) {
+		return isSendInlineImages(htmlMessage, inlineImages) && 
+				isSendAttachments(attachments);
+	}
+	
+	/**
+	 * メッセージをJISに変換
+	 * @param msg メッセージ
+	 * @return メッセージを変換した文字列
+	 */
+	public static String convertJIS(String msg) {
 		try {
 			byte[] convBytes = msg.getBytes(CHARSET);
 			return new String(convBytes);
