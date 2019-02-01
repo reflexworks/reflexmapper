6db996be6dc9ceb2a78abde4e78c30b66afdb326
takezaki <takezaki@virtual-tech.net>
2019-02-01 16:24:16 +0900

    startArrayBracketの非static化

--- src/main/java/jp/reflexworks/atom/entry/FeedBase.java
+++ src/main/java/jp/reflexworks/atom/entry/FeedBase.java
@@ -17,7 +17,6 @@ import org.msgpack.annotation.Index;
 public abstract class FeedBase implements Serializable, Cloneable {
 
 	private static final long serialVersionUID = 1L;	
-	private static boolean startArrayBracket = false;
 	
 	/**
 	 * デフォルトの名前空間
@@ -61,12 +60,15 @@ public abstract class FeedBase implements Serializable, Cloneable {
 	@Index(14)
 	public List<EntryBase> entry;
 
+	private Boolean startArrayBracket;
+
 	public boolean getStartArrayBracket() {
+		if (startArrayBracket==null) return true;
 		return startArrayBracket;
 	}
 	
 	public void setStartArrayBracket(boolean startArrayBracket) {
-		FeedBase.startArrayBracket=startArrayBracket;
+		this.startArrayBracket=startArrayBracket;
 	}
 	
 	public List<Author> getAuthor() {
